27a72d59418de4b354bc6d711c2815b4c98c433e
Bam4d <chrisbam4d@gmail.com>
2022-12-21 20:19:07 +0000

    example of failing to build with dependencies

--- EmscriptenWrapper/EmscriptenWrapper.cpp
+++ EmscriptenWrapper/EmscriptenWrapper.cpp
@@ -1,9 +1,28 @@
 #include "EmscriptenWrapper.hpp"
 
+EmscriptenWrapper::EmscriptenWrapper() {}
 
-EmscriptenWrapper::EmscriptenWrapper() {
-  
+std::string EmscriptenWrapper::ZlibVersion() {
+  return std::string(zlibVersion());
 }
 
+std::string EmscriptenWrapper::GLMVersion() {
 
-std::string EmscriptenWrapper::ZlibVersion() { return std::string(zlibVersion()); }
+  auto vec = glm::ivec2(0.1, 0.2);
+
+  if (vec.x == 0.1) {
+    return std::string("test");
+  }
+
+  return "test";
+}
+
+std::string EmscriptenWrapper::YAMLVersion() {
+  YAML::Node node;
+
+  if (node.IsDefined()) {
+    return "test";
+  }
+
+  return "test";
+}
